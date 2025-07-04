dab632da06806900f22a398ee63de7f281d010bc
anasavastre <anamaria.savastre10@gmail.com>
2025-07-04 21:27:19 +0100

    functions - refactoring documentation

--- functions.py
+++ functions.py
@@ -3,6 +3,16 @@ from maya import cmds as mc
 
 def get_closest_UV_on_Surface(nrbSurface, position):
     '''
+    This function will return the closes UV parameter to a specified position
+
+    Parameters
+    ----------
+    nrbSurface  : str : name of the nurbs surface
+    position    : list or MPoint: provide the X, Y, Z coordinates of the position we are querying 
+
+    Returns
+    -------
+    u, v : double : the closest U, V parameters on the surface
     '''
     if isinstance(position, list):
         position = om.MPoint(position)
@@ -21,7 +31,29 @@ def get_closest_UV_on_Surface(nrbSurface, position):
 
 def rivet_to_surface(nrbSurface, transform, u, v):
     '''
-    PointOnSurfaceInfo -> Matrix -> object.offsetParentMatrix
+    This function will construct a rivet set-up, using a point on surface info node to extract surface normal and tangents, and a four by four matrix to construct our rivet transform matrix. 
+
+    The 4X4 matrix will have the following structure:
+        [
+            [surfaceTangentU.X, surfaceTangentU.Y, surfaceTangentU.Z, 0],
+            [surfaceNormal.X, surfaceNormal.Y, surfaceNormal.Z, 0],
+            [biNormal.X, biNormal.Y, biNormal.Z, 0],
+            [position.X, position.Y, position.Z, 1]
+        ], 
+    where the 
+            biNormal = surfaceTangent ^ surfaceNormal 
+            position = world space position at parameters u, v
+    
+    Parameters
+    ----------
+    nrbSurface  : str : name of the nurbs surface
+    transform   : str : name of transform we want to rivet
+    u, v        : double: parameter at which we are constructing the rivet 
+
+    Returns
+    -------
+    None
+
     '''
 
     pointOnSurface = mc.createNode('pointOnSurfaceInfo',  name=transform[:-3]+'PSI')
@@ -53,7 +85,19 @@ def rivet_to_surface(nrbSurface, transform, u, v):
     
 def min(name, attribute, value):
     '''
-    For simplicity we will take the min between an attr and a value
+    Returning the minimum between our attribute and a provided value
+    -> min(attribute, value)
+
+    Parameters
+    ----------
+    name        : str : name to assign to the condition node
+    attribute   : str : name of attribute we want to construct the minimum for 
+    value       : double: min value we compare against 
+
+    Returns
+    -------
+    conditionNode: str 
+    
     '''
 
     minNode = mc.createNode('condition', name=name)
@@ -65,34 +109,54 @@ def min(name, attribute, value):
         mc.setAttr(minNode+'.secondTerm', value)
     else:
         mc.connectAttr(value, minNode+'.colorIfFalseR')
-        mc.connectAttr(value, minNode+'.secondTerm')
-    
+        mc.connectAttr(value, minNode+'.secondTerm')    
     
     return minNode
 
 
 def max(name, attribute, value):
     '''
-    For simplicity we will take the max between an attr and a value
+    Returning the max between our attribute and a provided value
+    -> max(attribute, value)
+
+    Parameters
+    ----------
+    name        : str : name to assign to the condition node
+    attribute   : str : name of attribute we want to construct the maximum for 
+    value       : double: max value we compare against 
+
+    Returns
+    -------
+    conditionNode: str 
     '''
 
-    minNode = mc.createNode('condition', name=name)
-    mc.connectAttr(attribute, minNode+'.firstTerm') 
-    mc.connectAttr(attribute, minNode+'.colorIfTrueR') 
-    mc.setAttr(minNode+'.operation', 2 )
+    maxNode = mc.createNode('condition', name=name)
+    mc.connectAttr(attribute, maxNode+'.firstTerm') 
+    mc.connectAttr(attribute, maxNode+'.colorIfTrueR') 
+    mc.setAttr(maxNode+'.operation', 2 )
     if isinstance(value, float) or isinstance(value, int):
-        mc.setAttr(minNode+'.colorIfFalseR', value)
-        mc.setAttr(minNode+'.secondTerm', value)
+        mc.setAttr(maxNode+'.colorIfFalseR', value)
+        mc.setAttr(maxNode+'.secondTerm', value)
     else:
-        mc.connectAttr(value, minNode+'.colorIfFalseR')
-        mc.connectAttr(value, minNode+'.SecondTerm')
+        mc.connectAttr(value, maxNode+'.colorIfFalseR')
+        mc.connectAttr(value, maxNode+'.SecondTerm')
     
     
-    return minNode
+    return maxNode
 
 def subtract(name, attribute, value):
     '''
-    
+    Returns attribute - value
+
+    Parameters
+    ----------
+    name        : str : name to assign to the node
+    attribute   : str : name of attribute we want to subtract from 
+    value       : double: value to subtract 
+
+    Returns
+    -------
+    subtractNode: str
     '''
 
     node = mc.createNode('subtract', name=name)
@@ -106,6 +170,19 @@ def subtract(name, attribute, value):
     return node
 
 def multiply_Double(name, attribute, value):
+    '''
+    Returns attribute * value
+
+    Parameters
+    ----------
+    name        : str : name to assign to the node
+    attribute   : str : name of attribute we want to multiply from 
+    value       : double: value to subtract 
+
+    Returns
+    -------
+    subtractNode: str
+    '''
     node = mc.createNode('multDoubleLinear', name=name)
     mc.connectAttr(attribute, node+'.input1') 
 
